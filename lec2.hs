2f3f4b30dad85dec7dc27a5c510fba57d376ee08
jocespitia <jocelyn.espitia@gmail.com>
2020-02-06 21:46:26 -0800

    playing around with funcitons

--- lec2.hs
+++ lec2.hs
@@ -1,3 +1,5 @@
---twice2 = \n -> 2 * n
-
 doubleMe x = x + x
+doubleUs x y = x*2 + y*2 --this means that in the terminal you can do doubleUs 4 9 and get 26
+--cuz 4*2 = 8 and 9*2 = 18
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
